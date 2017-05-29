e586b414adac6aa3f6c8a00575a76c49b8931315
Ahmad Fajar <shin.fajar@gmail.com>
2017-05-29 07:35:45 +0700

    ahmad fajar - sudoku 1

--- sudoku.js
+++ sudoku.js
@@ -1,13 +1,40 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this.arr = board_string.match(/\d{9}/g)
+  }
 
   solve() {}
 
   // Returns a string representing the current state of the board
-  board() {}
-}
+  board() {
+    const horLine = '---------------------\n';
+    let arr = this.arr;
+    let sudoString = horLine;
+
+    for (var row = 0; row < 9; row++) {
+      for (var col = 0; col < 9; col++) {
+        if (col + 1 === 9) {
+          sudoString += `${arr[row][col]}`;
+        } else if (((col + 1) % 3 === 0 ) && (col < 8)) {
+          sudoString += `${arr[row][col]} | `;
+        } else {
+          sudoString += `${arr[row][col]} `;
+        }
+      }
+
+      if ((row + 1) % 3 === 0) {
+        sudoString += `\n${horLine}`;
+      } else {
+        sudoString += '\n';
+      }
+    }
+
+    return sudoString;
+  }
+
+}  // --------------- end of line ---------------
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
@@ -19,6 +46,8 @@ var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
 var game = new Sudoku(board_string)
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
 
+// console.log(game.arr);
+
+game.solve()
 console.log(game.board())
