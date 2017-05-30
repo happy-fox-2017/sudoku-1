a07e93c2fae0a15f3611c1d70e52ca09469be2ba
kurei <kokohtan@gmail.com>
2017-05-30 11:28:21 +0700

    add sudoku1

--- sudoku.js
+++ sudoku.js
@@ -2,72 +2,63 @@
 
 class Sudoku {
   constructor(board_string) {
+    this.boardStringDigitArray = board_string.match(/\d{9}/g);
+    this.game = this.boardStringDigitArray.map(function(array) {
+      array = array.split("");
+      return array;
+    })
     this.fullNumArray = "123456789".split("");
-    this.game = this.arrangeBoard(board_string);
-  }
-
-this.arrangeBoard(str) {
-  let regexDigit = /\d{9}/g;
-  let arrayAwal = str.match(regexDigit);
-  let boardFinal = [];
-  for (let i = 0; i < arrayAwal.length; i++) {
-      let Hor =[]
-      for (let j=0; j < arrayAwal[i].length; j++){
-        Hor.push(+this.arrayAwal[i][j]);
-      }
-      boardFinal.push(Hor);
-    }
-    return boardFinal;
-  } 
-
- board() {
-    let Line = "=====================";
-    let allBoard = "";
-    allBoard += Line +"\n" ;
-    for (let i=0; i<this.game.length; i++) {
-      for (let j=0; j<3; j++) {
-        let correctNum = this.game[i].slice(j,j+3).join(" ");
-           allBoard += correctNum;
-        if (j!=2) {
-          allBoard += " | " };
-      }
-      allBoard += "\n";
-      if (i%3===0) {
-        allBoard += Line + "\n"};
-    }
-    return allBoard;
-  }
-
-  // board clear
-
-  check(Horizontal, Vertical, hitNum){
-    let H=false, V=true, D=true;
-    H = !this.game[Horizontal].includes(hitNum);
-    for (let i=0; i<this.game.length; i++) {
-      if (this.game[i][Vertical] == hitNum){
-        V = false;
-      }
-    }
   }
 
   solve() {
     for (let i=0; i<this.game.length; i++) {
-          console.log(this.board());
-        for (let hitNum in this.fullNumArray) {
-             if (this.check(i,j,this.fullNumArray[hitNum])) {
+      for (let j=0; j<this.game[0].length; j++) {
+        //  console.log(this.board());
+        if (this.game[i][j] == 0) {
+          for (let hitNum in this.fullNumArray) {
+            if (this.check(i,j,this.fullNumArray[hitNum])) {
               this.game[i][j] = this.fullNumArray[hitNum];
-              let gameEnd = this.solve();
-              if (gameEnd === true) 
-                {return true;
-              this.game[i][j] = 0;
+              let allCheck = this.solve();
+              if (allCheck === true) return true;
+                this.game[i][j] = 0;
             }
           }
-        } return false;
-      }  return true;
+          return false;
+        }
+      }
     }
-  
-}
+     console.log(this.board());
+    return true;
+  }
 
+  check(Horizontal, Vertical, hitNum){
+    let H=false, V=true, D = true;
+    H = !this.game[Horizontal].includes(hitNum);
+    for (let i=0; i<this.game.length; i++) {
+      if (this.game[i][Vertical] == hitNum){
+        V = false;
+        break;
+      }
+    }
+    return H && V && D;
+  }
+
+  board() {
+    let allBoard = "";
+    let Line = "=====================";
+    allBoard += Line +"\n" ;
+    for (let i=0; i<this.game.length; i++) {
+      for (let j=0; j<3; j++) {
+        let onePlace = this.game[i].slice(j*3,j*3+3).join(" ");
+        allBoard += onePlace;
+        if (j!=2) allBoard += " | ";
+      }
+      allBoard += "\n";
+      if ((i+1)%3===0) allBoard += Line + "\n";
+    }
+    return allBoard;
+  }
+}
 
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
@@ -76,9 +67,11 @@ this.arrangeBoard(str) {
 //   .toString()
 //   .split("\n")[0]
 
-var game = new Sudoku(005030081902850060600004050007402830349760005008300490150087002090000600026049503)
-
-// Remember: this will just fill out what it can and not "guess"
+var board_string = "302609005500730000000000900000940000000000109000057060008500006000000003019082040"
+var game = new Sudoku(board_string)
 game.solve()
 
-//console.log(game.board())
+
+
+
+
